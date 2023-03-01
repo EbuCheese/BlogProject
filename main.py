4712d9e88acf389fbeb309269efbf3f6661a399e
ebucheese <ethandonate@gmail.com>
2023-02-28 16:40:48 -0800

    sadsadasdas

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -256,4 +256,4 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run() #host='0.0.0.0', port=5000
